3cf52482db32e366e0f18cc4451168c2f56e759e
ArchieS23 <19212618@brookes.ac.uk>
2026-05-07 18:22:43 +0100

    plan now gives you a tempo or interval run aswell as easy runs, I think one interval or tempo run is enough for a week for now but maybe people who run 6-7 times a week need more, this is something I will have to research so I can make plans for different levels of runners. I've also created the lists of some intervals and tempo runs that I can add in and tested it by adding in the interval types.

--- PlanPage.py
+++ PlanPage.py
@@ -23,26 +23,51 @@ class PlanPage(Screen):
 
         #Days in the week
         days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
+        hard_run_types = ["Interval Run", "Tempo Run"]
+        interval_types = [
+            '1KM Repeats',
+            'Mile Repeats',
+            'Pyramid Intervals'
+        ]
+        tempo_types =  [
+            'Tempo 3-2-1',
+            'Tempo 2KM Repeats',
+            'Over and under 1KMs',
+            'Tempo 5KM',
+            'Rolling 400s',
+            'Tempo 4KM'
+        ]
+        run_index = 0
+        weekly_hard_run = 0
         #Dictionary for the plan.
         plan = {}
 
         #For week in range 1 to the length of the current plan
         for week in range(1, plan_length + 1):
             #Create nested dictionary for each week in range
-            plan[f"Week {week}"] = {}
+            week_name = f"Week {week}"
+            plan[week_name] = {}
             #Create day for each day in the week
             for day in days:
                 #For each day if an activity day the value of that day will = 'run'
-                if day in activity_days and day is not long_run_day:
-                    plan[f"Week {week}"][day] = "Run"
+                if day == long_run_day:
+                    plan[week_name][day] = "Long Run"
                 #For each day if it is a long run day the value of that day will = 'long run'
-                elif day == long_run_day:
-                    plan[f"Week {week}"][day] = "Long Run"
+                elif day in activity_days:
+                    plan[week_name][day] = {hard_run_types[run_index % len(hard_run_types)],
+                                            interval_types[run_index % len(interval_types)]}
+                    run_index += 1
+
+                    if weekly_hard_run > 0:
+                        plan[week_name][day] = "Easy Run"
+                    weekly_hard_run += 1
+
+
                 #Else the value of the day will be rest
                 else:
-                    plan[f"Week {week}"][day] = "Rest"  # default value
+                    plan[week_name][day] = "Rest"  # default value
 
+            weekly_hard_run = 0
 
         print(plan)
 
