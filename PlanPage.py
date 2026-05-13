8e418563bf27242d795c46c3f06283144389ab5a
ArchieS23 <19212618@brookes.ac.uk>
2026-05-13 22:06:01 +0100

    Implemented better logic for the plan, it uses the race and the longest run of the user. long run uses longest run + week number * 1.5 to keep progressing. This does not factor in how many weeks the plan is as this is a major factor in how long their long run is. The easy run is based off of weekly milage but does not work well right now as it would just be the same easy run week after week, I will have to update this logic aswell.

--- PlanPage.py
+++ PlanPage.py
@@ -18,6 +18,10 @@ class PlanPage(Screen):
     def on_enter(self):
         data = App.get_running_app().data
         plan_length = int(data.get("CurrentPlanLength", 0))
+        PB = float(data.get("CurrentPB", 0))
+        race = data.get("race")
+        longest_run = int(data.get("Longest_Run"))
+        weekly_miles = int(data.get("Weekly_Distance"))
         activity_days = data.get("ActivityDays")
         long_run_day = data.get("LongRunDay")
 
@@ -37,36 +41,79 @@ class PlanPage(Screen):
             'Rolling 400s',
             'Tempo 4KM'
         ]
+        race_run_types = {"5k": 5, "10k": 10, "half": 21.1, "marathon": 42.2}
         run_index = 0
+        long_run_progression = 0
         weekly_hard_run = 0
+        value = race_run_types[race]
+
         #Dictionary for the plan.
         plan = {}
 
         #For week in range 1 to the length of the current plan
         for week in range(1, plan_length + 1):
-            #Create nested dictionary for each week in range
             week_name = f"Week {week}"
             plan[week_name] = {}
-            #Create day for each day in the week
-            for day in days:
-                #For each day if an activity day the value of that day will = 'run'
-                if day == long_run_day:
-                    plan[week_name][day] = "Long Run"
-                #For each day if it is a long run day the value of that day will = 'long run'
-                elif day in activity_days:
-                    plan[week_name][day] = {hard_run_types[run_index % len(hard_run_types)],
-                                            interval_types[run_index % len(interval_types)]}
-                    run_index += 1
+            # Alternate workout type each week
+            hard_type = hard_run_types[week % len(hard_run_types)]
 
-                    if weekly_hard_run > 0:
-                        plan[week_name][day] = "Easy Run"
+            # Select actual session
+            if hard_type == "Interval Run":
+                session = interval_types[week % len(interval_types)]
+            else:
+                session = tempo_types[week % len(tempo_types)]
+
+            for day in days:
+                # Default
+                plan[week_name][day] = {
+                    "type": "Rest"
+                }
+                # Long run
+                if day == long_run_day:
+                    #Round to the nearest whole number
+                    long_run_distance = round(
+                        longest_run + (week * 1.5),
+                        1
+                    )
+
+                    # Prevent overreaching
+                    if long_run_distance > value * 0.8:
+                        long_run_distance = round(value * 0.8, 1)
+
+                    #Create nested dict to store everything for the run
+                    plan[week_name][day] = {
+                        "type": "Long Run",
+                        "distance": long_run_distance
+                    }
+
+                # Other running days
+                elif day in activity_days:
+
+                    # First activity day = hard session
+                    if weekly_hard_run == 0:
+
+                        # Create nested dict to store everything for the run
+                        plan[week_name][day] = {
+                            "type": hard_type,
+                            "session": session
+                        }
+
+                    # Remaining = easy runs
+                    else:
+                        # Round to the nearest whole number
+                        easy_distance = round(
+                            weekly_miles / len(activity_days) * 0.6,
+                            1
+                        )
+                        # Create nested dict to store everything for the run
+                        plan[week_name][day] = {
+                            "type": "Easy Run",
+                            "distance": easy_distance
+                        }
+                    #No more then one hard run a week right now.
                     weekly_hard_run += 1
 
-
-                #Else the value of the day will be rest
-                else:
-                    plan[week_name][day] = "Rest"  # default value
-
+            # Reset for next week
             weekly_hard_run = 0
 
         print(plan)
