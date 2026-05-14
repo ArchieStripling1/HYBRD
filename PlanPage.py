175569cc4e6b11a821ed6eb08e0576c0a0cfbb51
ArchieS23 <19212618@brookes.ac.uk>
2026-05-14 17:54:02 +0100

    added Race Setting which sets up the max long run. race pace can be calculated and changed for an easy pace tempo pace and interval pace aswell but does not work yet as needs to be sorted into hours/minutes/seconds. added a recovery week for every 4th week which cuts the weekly mileage by 20%. distance to cover means if they have 3 weeks and need to get their distance up to race distance; it will go up accordingly (so very drastically) and the same if it was 20 weeks (so very slow). cleaner code and workout pace for the interval and tempo sessions. added workouts so that I can output the dictionaires in a cleaner way

--- PlanPage.py
+++ PlanPage.py
@@ -18,16 +18,16 @@ class PlanPage(Screen):
     def on_enter(self):
         data = App.get_running_app().data
         plan_length = int(data.get("CurrentPlanLength", 0))
-        PB = float(data.get("CurrentPB", 0))
+        PB = float(data.get("CurrentPB", 2))
         race = data.get("race")
         longest_run = int(data.get("Longest_Run"))
         weekly_miles = int(data.get("Weekly_Distance"))
         activity_days = data.get("ActivityDays")
         long_run_day = data.get("LongRunDay")
+        weekly_hard_run = 0
 
         #Days in the week
         days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        hard_run_types = ["Interval Run", "Tempo Run"]
         interval_types = [
             '1KM Repeats',
             'Mile Repeats',
@@ -41,50 +41,92 @@ class PlanPage(Screen):
             'Rolling 400s',
             'Tempo 4KM'
         ]
-        race_run_types = {"5k": 5, "10k": 10, "half": 21.1, "marathon": 42.2}
-        run_index = 0
-        long_run_progression = 0
-        weekly_hard_run = 0
-        value = race_run_types[race]
+        race_settings = {
+            "5k": {
+                "max_long_run" : 12
+            },
+            "10k": {
+                "max_long_run" : 18
+            },
+            "half": {
+                "max_long_run" : 28
+            },
+            "marathon": {
+                "max_long_run" : 34
+            }
+        }
+        if race == "5k":
+            race_pace = PB / 5
+        elif race == "10k":
+            race_pace = PB / 10
+        elif race == "half":
+            race_pace = PB / 21.1
+        elif race == "marathon":
+            race_pace = PB / 42.2
+
+        easy_pace = round(race_pace + 1.2, 2)
+        tempo_pace = round(race_pace + 0.25, 2)
+        interval_pace = round(race_pace - 0.15, 2)
 
         #Dictionary for the plan.
         plan = {}
 
         #For week in range 1 to the length of the current plan
         for week in range(1, plan_length + 1):
-            week_name = f"Week {week}"
-            plan[week_name] = {}
-            # Alternate workout type each week
-            hard_type = hard_run_types[week % len(hard_run_types)]
 
-            # Select actual session
-            if hard_type == "Interval Run":
-                session = interval_types[week % len(interval_types)]
+            week_name = f"Week {week}"
+
+            plan[week_name] = {}
+
+            current_weekly_distance = weekly_miles
+
+            #Created a recovery week every 4 weeks which decrease the load
+            recovery_week = False
+
+            if week % 4 == 0:
+                recovery_week = True
+                current_weekly_distance *= 0.8
             else:
-                session = tempo_types[week % len(tempo_types)]
+                current_weekly_distance *= 1.05
+
+            # Alternate workout type each week
+
 
             for day in days:
                 # Default
-                plan[week_name][day] = {
+                workout = {
                     "type": "Rest"
                 }
+                plan[week_name][day] = workout
+
                 # Long run
                 if day == long_run_day:
-                    #Round to the nearest whole number
-                    long_run_distance = round(
-                        longest_run + (week * 1.5),
-                        1
+                    distance_to_cover = (
+                            race_settings[race]["max_long_run"] - longest_run
                     )
 
-                    # Prevent overreaching
-                    if long_run_distance > value * 0.8:
-                        long_run_distance = round(value * 0.8, 1)
+                    weekly_increase = (
+                            distance_to_cover / max(plan_length - 2, 1)
+                    )
 
-                    #Create nested dict to store everything for the run
-                    plan[week_name][day] = {
+                    long_run_distance = (
+                            longest_run +
+                            (weekly_increase * week)
+                    )
+
+                    # Prevent going over max
+                    if long_run_distance > race_settings[race]["max_long_run"]:
+                        long_run_distance = race_settings[race]["max_long_run"]
+
+                    if recovery_week:
+                        long_run_distance *= 0.8
+
+                    workout = {
                         "type": "Long Run",
-                        "distance": long_run_distance
+                        "distance": long_run_distance,
+                        "pace": easy_pace
                     }
+                    plan[week_name][day] = workout
 
                 # Other running days
                 elif day in activity_days:
@@ -92,31 +134,66 @@ class PlanPage(Screen):
                     # First activity day = hard session
                     if weekly_hard_run == 0:
 
+                        # Select actual session
+                        if week % 2 == 0:
+                            hard_type = "Tempo Run"
+
+                            session = tempo_types[
+                                week % len(tempo_types)
+                                ]
+
+                            workout_pace = tempo_pace
+
+                        else:
+
+                            hard_type = "Interval Run"
+
+                            session = interval_types[
+                                week % len(interval_types)
+                                ]
+
+                            workout_pace = interval_pace
+
                         # Create nested dict to store everything for the run
-                        plan[week_name][day] = {
+                        workout = {
                             "type": hard_type,
-                            "session": session
+                            "session": session,
+                            "pace": workout_pace
                         }
+                        plan[week_name][day] = workout
 
                     # Remaining = easy runs
                     else:
                         # Round to the nearest whole number
-                        easy_distance = round(
+                        easy_distance = int(round(
                             weekly_miles / len(activity_days) * 0.6,
                             1
-                        )
+                        ))
                         # Create nested dict to store everything for the run
-                        plan[week_name][day] = {
+                        workout = {
                             "type": "Easy Run",
-                            "distance": easy_distance
+                            "distance": easy_distance,
+                            "pace": easy_pace
                         }
-                    #No more then one hard run a week right now.
+                        plan[week_name][day] = workout
+
+                    #No more than one hard run a week right now.
                     weekly_hard_run += 1
+
 
             # Reset for next week
             weekly_hard_run = 0
 
-        print(plan)
+        App.get_running_app().data["GeneratedPlan"] = plan
+
+
+        for week, workouts in plan.items():
+            print(f"\n======== {week} ========")
+
+            for day, workout in workouts.items():
+                print(day, "->", workout)
+
+
 
 
 
